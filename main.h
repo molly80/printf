3530e6a11aee550d1c8517e75e1b5812cda8ddfd
molly80 <maureenwangui09@gmail.com>
2022-05-21 00:41:23 -0700

    defining specifiers in c

--- main.h
+++ main.h
@@ -4,10 +4,10 @@ int _printf(const char *format, ...);
 void vprintk( const char* fmt, va_list args);
 int _printchar(va_list args);
 int _printstr(va_list args);
-
-
-
-
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _validate_char(char type);
 
 
 #endif
