0ea7a1529a1e508ed18bd428d0baf2b24c063d92
molly80 <maureenwangui09@gmail.com>
2022-05-21 06:38:48 -0700

    update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -9,5 +9,18 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buffer, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buffer[i] = c;
+		i++;
+	}
+	return (1);
 }
